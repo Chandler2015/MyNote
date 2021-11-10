9a72b17b3c8ec6d2b16e6b6f179c3b7ff1931805
YuhangXiong <yxiong4@ualberta.ca>
2021-11-10 01:38:52 -0700

    added rest_framework with getnotes and getnote view method and integrate with rest framework

--- Notes/views.py
+++ Notes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
 
+@api_view(['GET'])
 def getRoutes(request):
     # return HttpResponse('Hello world!')
     routes = [
@@ -36,4 +41,21 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes, safe=False)
+    # return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    # need to serialize the Notes to convert to Json format then can passed to Response
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    # need to serialize the Notes to convert to Json format then can passed to Response
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
