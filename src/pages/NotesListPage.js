77d0d5ab8dcc898ad08e1f17e5b7b8f8fec33a28
YuhangXiong <yxiong4@ualberta.ca>
2021-11-13 17:16:10 -0700

    connect django backend and fetch the notes data from rest api

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React , {useEffect, useState} from 'react'
 
 const NotesListPage = () => {
+    //init the notes = [], use setNotes to update the notes 
+    let [notes, setNotes] = useState([])
+
+    //when first get to the state
+    useEffect(() => {
+        getNotes()
+    }, []) 
+
+    let getNotes = async () => {
+        console.log("testing the react log")
+        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let data = await response.json()
+        
+        setNotes(data)
+    }
+
     return (
         <div>
-            NotesList
+           
+            <div className = "notes-list">
+                {notes.map((note, index) => (
+                    <h3>{note.body}</h3>
+                ))
+                }
+            </div>
         </div>
     )
 }
